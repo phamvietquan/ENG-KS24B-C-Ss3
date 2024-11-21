a010f67664133f27193eb7c1496ea909442199ed
phamvietquan <phamquanlv123.2986@gmail.com>
2024-11-22 00:46:31 +0700

    Update b7ss3.cpp

--- b7ss3.cpp
+++ b7ss3.cpp
@@ -4,11 +4,11 @@ int main(){
 	int so;
 	printf(" Moi ban nhap vao so nguyen co 4 chu so ");
 	scanf("%d", &so);
-	int tong = 0;
-	while (so > 0) {
-            tong += so % 10; 
-            so /= 10;        
-        }
+	int a = (so/1000)%10;
+	int b = (so/100)%10;
+	int c = (so/10)%10;
+	int d = so%10;
+	int tong = a + b + c + d;
 	printf(" tong cua 4 chu so ban da nhap la: %d", tong);
 	return 0;
 }
